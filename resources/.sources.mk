49abee4bcdb88d245ddd513b7101670fc857bacb
ondra-novak <ondra-novak@email.cz>
2015-10-12 00:55:03 +0200

    building improved

--- resources/.sources.mk
+++ resources/.sources.mk
@@ -1,17 +1,20 @@
-OBJS+=resources/rpc_js.o resources/wsrpc_js.o
+OBJS+=tmp/resources/rpc_js.o tmp/resources/wsrpc_js.o
+clean_list+=tmp/bin2c tmp/resources/wsrpc_js.c tmp/resources/rpc_js.c
 
 
-.INTERMEDIATE : resources/rpc_js.c resources/wsrpc_js.c bin2c
+#.INTERMEDIATE : tmp/resources/rpc_js.c tmp/resources/wsrpc_js.c tmp/bin2c
 
-bin2c: resources/bin2c.c
+
+tmp/bin2c: resources/bin2c.c
 	@echo Building resources
-	@$(CC) -o $@ resources/bin2c.c
+	mkdir -p tmp/resources
+	$(CC) -o $@ resources/bin2c.c
 
-resources/rpc_js.c: resources/rpc.js bin2c
-	@./bin2c resources/rpc.js jsonrpcserver_rpc_js > $@
+tmp/resources/rpc_js.c: resources/rpc.js tmp/bin2c
+	@tmp/bin2c resources/rpc.js jsonrpcserver_rpc_js > $@
 	
-resources/wsrpc_js.c: resources/wsrpc.js bin2c	
-	@./bin2c resources/wsrpc.js jsonrpcserver_wsrpc_js > $@
+tmp/resources/wsrpc_js.c: resources/wsrpc.js tmp/bin2c	
+	@tmp/bin2c resources/wsrpc.js jsonrpcserver_wsrpc_js > $@
 
 # prevent to apply default rule js.c to js as make thinks that js is executable
 %.js : %.js.c
